b8e1dd0e3ad796359b5f0a2656637fe072e73888
Chiragitr <chirag.guptaitr@gmail.com>
2019-10-22 21:28:54 +0530

    template completed

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,11 +1,43 @@
+import random
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 # FBV views
 def home(request):
-	html_= """
-		<h1>Heloo</h1>
-		<p>This is nice!! we can place html inside views,\n is it good Practice?</p>
-	"""
-	return HttpResponse(html_)
+	#return HttpResponse(html_)
+	num = random.randint(0,10000)
+	list = [123,random.randint(0,10000),random.randint(0,10000),random.randint(0,10000)]
+	context_data = {
+		"name":"Chirag Gupta",
+		"Place":"Chennai",
+		"num":num,"bool":True,
+		"list":list
+	}
+	return render(request, "base.html",context_data)
+
+
+def contact(request):
+	#return HttpResponse(html_)
+	num = random.randint(0,10000)
+	list = [123,random.randint(0,10000),random.randint(0,10000),random.randint(0,10000)]
+	context_data = {
+		"name":"Chirag Gupta",
+		"Place":"Chennai",
+		"num":num,"bool":True,
+		"list":list
+	}
+	return render(request, "contact.html",context_data)
+
+def about(request):
+	#return HttpResponse(html_)
+	num = random.randint(0,10000)
+	list = [123,random.randint(0,10000),random.randint(0,10000),random.randint(0,10000)]
+	context_data = {
+		"name":"Chirag Gupta",
+		"Place":"Chennai",
+		"num":num,"bool":True,
+		"list":list
+	}
+	return render(request, "about.html",context_data)
